bbf44382e2ff5e0ca5e7412938ee69a272c2ed0f
Nirmal7073 <nirmalpatel7073@gmail.com>
2024-07-15 17:29:56 +0530

    control

--- 03_Control Structures/control.js
+++ 03_Control Structures/control.js
@@ -19,7 +19,7 @@ valCheck(10);
 valCheck(-10);
 
 //     Task 2: Write a program to check if a person is eligible to vote (age >= 18) and log the result to the console.
-
+~~~
 function eligibleAge(age){
     if(age >= 18){
         console.log('person is eligible');
